ea0e15de2b0fda90322ca89f27088f89c7e52d0d
wangzunmin <15156191096@163.com>
2020-07-10 15:25:36 +0800

    走马灯效果

--- src/components/home/AppIndex.vue
+++ src/components/home/AppIndex.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>这是首页</div>
+  <div>
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,19 @@
 </script>
 
 <style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
 
+  .el-carousel__item:nth-child(2n) {
+    background:url("../../assets/eva.jpg");
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
